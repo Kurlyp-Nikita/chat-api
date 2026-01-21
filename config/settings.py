2ede1f981ad79b1a1ecd0e1d1dc9a5de15333e6f
Nikita <ilinnikita244@gmail.com>
2026-01-21 22:52:17 +0300

    chore: switch to SQLite to speed up development

--- config/settings.py
+++ config/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'chat_db',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
