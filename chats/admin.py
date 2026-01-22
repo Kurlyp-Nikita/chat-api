b594eb8301877d9195aa0554edb752c1697c78dc
Nikita <ilinnikita244@gmail.com>
2026-01-22 18:00:09 +0300

    feat: register Chat and Message models in admin

--- chats/admin.py
+++ chats/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from chats.models import Chat, Message
 
-# Register your models here.
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at')
+    search_fields = ('title',)
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'chat', 'created_at')
+    list_filter = ('created_at',)
