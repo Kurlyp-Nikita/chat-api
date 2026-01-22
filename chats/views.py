b2e3efa79e60fc163beb3c37fee346a1f4883952
Nikita <ilinnikita244@gmail.com>
2026-01-22 19:08:14 +0300

    @csrf_exempt

--- chats/views.py
+++ chats/views.py
@@ -1,9 +1,11 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from chats.models import Message, Chat
 from django.shortcuts import get_object_or_404
 
 
+@csrf_exempt
 def create_chat(request):
     title = request.POST.get('title')
 
@@ -19,6 +21,7 @@ def create_chat(request):
     }, status=201)
 
 
+@csrf_exempt
 def create_message(request, chat_id):
 
     chat = get_object_or_404(Chat, id=chat_id)
@@ -37,6 +40,7 @@ def create_message(request, chat_id):
     })
 
 
+@csrf_exempt
 def get_chat(request, chat_id):
     chat = get_object_or_404(Chat, id=chat_id)
 
@@ -62,6 +66,7 @@ def get_chat(request, chat_id):
     })
 
 
+@csrf_exempt
 def delete_chat(request, chat_id):
     chat = get_object_or_404(Chat, id=chat_id)
     chat.delete()
